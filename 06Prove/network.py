6bd2cfa8be7245c50f3a0fd769399c4b3c963712
joshuchy <joshua.uchytil@gmail.com>
2017-02-22 22:37:50 -0700

    new additions

--- 06Prove/network.py
+++ 06Prove/network.py
@@ -13,6 +13,8 @@ class Network(object):
         self.layers = [[neuron.Neuron(temp[i+1]) for n in range(nodeCounts[i])] for i in range(numLayers)]
         self.pred = 0
         self.arr = []
+        self.target = 0
+        self.errorList = []
 
     #recursive : gets the activation and passes it on to the next level
     def calcLayer(self, inputs, layerNum=0):
@@ -27,32 +29,89 @@ class Network(object):
         #quit out if you hit the output layer
         if layerNum == len(self.layers):
             #for calculating prediction
-            self.pred = newInputs[1:].index(max(newInputs[1:]))
+            self.pred = newInputs[1:].index(max(newInputs[1:]))#omit the bias node
+
+            #activation of output layer calculates error
             self.arr = newInputs[1:]
             return
         else:
             self.calcLayer(newInputs, layerNum)
 
 
+    def calcError(self):
+
+        errorList = []
+        count = 0
+        for layer in reversed(self.layers): #bias nodes need the weights changes
+        #calc hidden layer
+            errors = []
+            if count == 0:
+                for l in layer:
+                    error = l.actv*(1 - l.actv)*(l.actv - self.target) #output layer error
+                    errors.append(error)
+            else:
+                for l in layer:
+                    sumW = 0
+                    for w,e in zip(l.weights,errorList[-1]):
+                        sumW += (w*e)
+                    error = l.actv*(1 - l.actv)*sumW
+                    errors.append(error)
+            count+=1
+            errorList.append(errors)
+        self.errorList = reversed(errorList)
+                            
+        #calc ouput layer
+            
+
+
+    def modWeights(self):
+
+        for layer in self.layers:
+            for l,error in zip(layer,self.errorList):
+                newWeights = []
+                #print l.weights
+                for w,e in zip(l.weights,error):
+                    new = w -(l.learn * e * l.actv)
+                    newWeights.append(new)
+                l.weights = newWeights
+                #print l.weights
+                    
+                    
+
+
 
 
 
 #getting data and user preferences
-data = csv.CSV().data
-numLayers = int(raw_input('how many layers?: '))
+data = csv.CSV().data[:10]
+np.seterr(all='ignore')
+numLayers = int(raw_input('how many hidden layers?: '))+1
 nodeCounts = []
 for x in range(numLayers):
     if x==numLayers-1:
-        nodeCounts.append(int(raw_input('nodes for output layer: ')))
+        nodeCounts.append(int(raw_input('how many targets: ')))
     else:
-        nodeCounts.append(int(raw_input('nodes for hidden layer ' + str(x+1) + ': '))) 
+        nodeCounts.append(int(raw_input('nodes for hidden layer ' + str(x+1) + ': '))+1)#add 1 for bias
+
+
+########################
 
 #initializing neural network
 n = Network(len(data[0]),numLayers, nodeCounts)
 predictions = []
-for row in data:
-    n.calcLayer(row[:-1])
-    predictions.append((n.pred, n.arr))
+
+for i in range(10000):
+    for row in data:
+        n.target = row[-1]
+        n.calcLayer([-1] + row[:-1])#omit the target answer
+        n.calcError()
+        n.modWeights()
+        if i == 9999:
+            predictions.append((n.pred, n.arr))
+
+
+
+########################
 
 #printing accuracy
 accuracy = 0
